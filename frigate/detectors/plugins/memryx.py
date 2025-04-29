3ce99f5a115054ee8ef47262104f0514f8c32b85
Abinila Siva <abinila.siva@memryx.com>
2025-04-29 14:20:13 -0400

    --add-host updated

--- frigate/detectors/plugins/memryx.py
+++ frigate/detectors/plugins/memryx.py
@@ -129,7 +129,7 @@ class MemryXDetector(DetectionApi):
             # Load MemryX Model with a unique device target
             self.accl = AsyncAccl(
                 self.memx_model_path,
-                mxserver_addr="172.17.0.1",
+                mxserver_addr="gateway.docker.internal",
                 group_id=device_id,  # AsyncAccl device id
             )
 
--- startdocker.sh
+++ startdocker.sh
@@ -15,6 +15,7 @@ sudo docker run -d \
   --shm-size=256m \
   -v /home/memryx/Frigate_MemryX/config:/config \
   -e FRIGATE_RTSP_PASSWORD='password' \
+  --add-host gateway.docker.internal:host-gateway \
   --privileged=true \
   -p 8971:8971 \
   -p 8554:8554 \
